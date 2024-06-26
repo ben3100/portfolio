e99c36dfa8953c72e2fe564b47cf239768a33b70
ben3100 <131266856+ben3100@users.noreply.github.com>
2024-06-26 23:39:45 +0200

    Update index.jsx

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -1,3 +1,10 @@
+import { motion } from "framer-motion";
+import { FaCode, FaWordpress, FaDatabase, FaEnvelope, FaNetworkWired, FaGraduationCap } from "react-icons/fa";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+import ServiceSlider from "../../components/ServiceSlider";
+import { fadeIn } from "../../variants";
+
 export const serviceData = [
   {
     title: "Développement Web",
@@ -30,3 +37,48 @@ export const serviceData = [
     icon: <FaGraduationCap />,
   },
 ];
+
+const Services = () => {
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
+              Mes services <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              Voici les services que je propose pour répondre à vos besoins technologiques et professionnels.
+            </motion.p>
+          </div>
+
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            <ServiceSlider services={serviceData} />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
+};
+
+export default Services;
