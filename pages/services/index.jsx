4780a45a68a185269942efc6c9d498c778edcc29
ben3100 <131266856+ben3100@users.noreply.github.com>
2024-06-26 23:47:33 +0200

    Update index.jsx

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -44,6 +44,7 @@ const Services = () => {
       <Circles />
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
             <motion.h2
               variants={fadeIn("up", 0.2)}
@@ -65,6 +66,7 @@ const Services = () => {
             </motion.p>
           </div>
 
+          {/* slider */}
           <motion.div
             variants={fadeIn("down", 0.6)}
             initial="hidden"
